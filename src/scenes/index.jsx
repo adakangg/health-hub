fc64a3b7995ac97fb219e190589520117957d385
adakang17 <adakang17@gmail.com>
2025-04-10 11:01:07 -0400

    initial commit

---
+++ src/scenes/index.jsx
@@ -0,0 +1,9 @@
+export { AttributionsPage } from "./attributions"; 
+export { ConditionsPage } from "./conditions"; 
+export { Dashboard } from "./dashboard";  
+export { HistoryPage } from "./history"; 
+export { MedicationsPage } from "./medications"; 
+export { CalendarPage } from "./calendar"; 
+export { VisitDetails } from "./visits/VisitDetails"; 
+export { VisitsPage } from "./visits";  
+export { WarningPage } from "./warning";  
